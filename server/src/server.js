3f329a945f54a78b00e9567c45db7252a531f1c9
lennygir <l.girardot74@gmail.com>
2023-10-21 21:18:20 +0200

    feat: data structure

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require("express");
+
+const app = new express();
+
+// --- Console requests
+app.use(function (req, res, next) {
+    console.log(`[${(new Date()).toLocaleTimeString("it-IT")}] - ${req.method} ${req.url}`);
+    next();
+});
+
+app.use(require("./routes.js"));
+
+module.exports = app;
