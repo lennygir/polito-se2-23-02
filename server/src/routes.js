f5ff904a8e17c44f6796f98bc5312a23078b8afb
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2023-10-22 18:53:12 +0200

    Added prettier formatter.

--- server/src/routes.js
+++ server/src/routes.js
@@ -26,44 +26,61 @@ router.get("/", (req, res) => {
  *  "servedClient": null
  * }
  */
-router.put('/counters/:counterId', (req, res) => {
-    // todo: missing type validation
-    const counter_targeted = dataService.data.counters.find(counter => parseInt(req.params.counterId) === counter.id);
-    if (counter_targeted === undefined) {
-        return res.status(403).send("The counter you want to modify does not exist");
-    }
-    if (!dataService.data.services.some(service => service.id === req.body.service)) {
-        return res.status(403).send("The service you want to add does not exist");
-    }
-    if (!counter_targeted.services.includes(req.body.service)) {
-        counter_targeted.services.push(req.body.service);
-    }
-    return res.json(counter_targeted);
-})
+router.put("/counters/:counterId", (req, res) => {
+  // todo: missing type validation
+  const counter_targeted = dataService.data.counters.find(
+    (counter) => parseInt(req.params.counterId) === counter.id,
+  );
+  if (counter_targeted === undefined) {
+    return res
+      .status(403)
+      .send("The counter you want to modify does not exist");
+  }
+  if (
+    !dataService.data.services.some(
+      (service) => service.id === req.body.service,
+    )
+  ) {
+    return res.status(403).send("The service you want to add does not exist");
+  }
+  if (!counter_targeted.services.includes(req.body.service)) {
+    counter_targeted.services.push(req.body.service);
+  }
+  return res.json(counter_targeted);
+});
 
 // removing a service from a counter that already hasn't it does not give an error
-router.delete('/counters/:counterId/services/:serviceId', (req, res) => {
-    const counter_targeted = dataService.data.counters.find(counter => parseInt(req.params.counterId) === counter.id);
-    if (counter_targeted === undefined) {
-        return res.status(403).send("The counter you want to modify does not exist");
-    }
-    if (!dataService.data.services.some(service => service.id === parseInt(req.params.serviceId))) {
-        return res.status(403).send("The service you want to remove does not exist");
-    }
-    counter_targeted.services = counter_targeted.services.filter(service => service.id !== parseInt(req.params.serviceId));
-    return res.json({});
-})
+router.delete("/counters/:counterId/services/:serviceId", (req, res) => {
+  const counter_targeted = dataService.data.counters.find(
+    (counter) => parseInt(req.params.counterId) === counter.id,
+  );
+  if (counter_targeted === undefined) {
+    return res
+      .status(403)
+      .send("The counter you want to modify does not exist");
+  }
+  if (
+    !dataService.data.services.some(
+      (service) => service.id === parseInt(req.params.serviceId),
+    )
+  ) {
+    return res
+      .status(403)
+      .send("The service you want to remove does not exist");
+  }
+  counter_targeted.services = counter_targeted.services.filter(
+    (service) => service.id !== parseInt(req.params.serviceId),
+  );
+  return res.json({});
+});
 
 // ==================================================
 // Handle 404 not found - DO NOT ADD ENDPOINTS AFTER THIS
 // ==================================================
 router.use(function (req, res) {
-  res
-    .status(404)
-    .json({
-      message:
-        "Endpoint not found, make sure you used the correct URL / Method",
-    });
+  res.status(404).json({
+    message: "Endpoint not found, make sure you used the correct URL / Method",
+  });
 });
 
 module.exports = router;
--- server/test/home.test.js
+++ server/test/home.test.js
@@ -13,111 +13,114 @@ describe("Testing the home endpoints", () => {
   });
 });
 
-describe('Adding a service into a counter', () => {
-    test('Putting an existing service into an existing counter', async () => {
-        await request(app)
-            .put('/counters/1')
-            .send({ service: 1 })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body.id).toBe(1);
-                expect(response.body.services).toContain(1);
-            });
-    });
-    test("Putting a service that doesn't exist", async () => {
-        await request(app)
-            .put('/counters/1')
-            .send({ service: 4 })
-            .expect('Content-Type', /html/)
-            .expect(403)
-            .then(response => {
-                expect(response.text).toBe("The service you want to add does not exist");
-            });
-    });
-    test("Trying to modify a counter that doesn't exist", async () => {
-        await request(app)
-            .put('/counters/5')
-            .send({ service: 1 })
-            .expect('Content-Type', /html/)
-            .expect(403)
-            .then(response => {
-                expect(response.text).toBe("The counter you want to modify does not exist");
-            });
-    });
-    test("Putting the same service into the same counter multiple times gives no duplication", async () => {
-        await request(app)
-            .put('/counters/2')
-            .send({ service: 1 })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body.id).toBe(2);
-                expect(response.body.services).toContain(1);
-                expect(response.body.services).toHaveLength(1);
-            });
-        await request(app)
-            .put('/counters/2')
-            .send({ service: 1 })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body.id).toBe(2);
-                expect(response.body.services).toContain(1);
-                expect(response.body.services).toHaveLength(1);
-            });
-    });
-    // todo: if there are clients being served the service insertion should not work
+describe("Adding a service into a counter", () => {
+  test("Putting an existing service into an existing counter", async () => {
+    await request(app)
+      .put("/counters/1")
+      .send({ service: 1 })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.id).toBe(1);
+        expect(response.body.services).toContain(1);
+      });
+  });
+  test("Putting a service that doesn't exist", async () => {
+    await request(app)
+      .put("/counters/1")
+      .send({ service: 4 })
+      .expect("Content-Type", /html/)
+      .expect(403)
+      .then((response) => {
+        expect(response.text).toBe(
+          "The service you want to add does not exist",
+        );
+      });
+  });
+  test("Trying to modify a counter that doesn't exist", async () => {
+    await request(app)
+      .put("/counters/5")
+      .send({ service: 1 })
+      .expect("Content-Type", /html/)
+      .expect(403)
+      .then((response) => {
+        expect(response.text).toBe(
+          "The counter you want to modify does not exist",
+        );
+      });
+  });
+  test("Putting the same service into the same counter multiple times gives no duplication", async () => {
+    await request(app)
+      .put("/counters/2")
+      .send({ service: 1 })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.id).toBe(2);
+        expect(response.body.services).toContain(1);
+        expect(response.body.services).toHaveLength(1);
+      });
+    await request(app)
+      .put("/counters/2")
+      .send({ service: 1 })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.id).toBe(2);
+        expect(response.body.services).toContain(1);
+        expect(response.body.services).toHaveLength(1);
+      });
+  });
+  // todo: if there are clients being served the service insertion should not work
 });
 
 describe("Removing a service from a counter", () => {
-    test("Removing an existing service from an existing counter", async () => {
-        // add service 1 into counter 2
-        await request(app)
-            .put('/counters/2')
-            .send({ service: 1 });
-        // remove service 1 from counter 2
-        await request(app)
-            .delete('/counters/2/services/1')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body).toStrictEqual({});
-            })
-    })
-    test("Removing a non existent service", async() => {
-        await request(app)
-            .delete('/counters/2/services/7')
-            .expect('Content-Type', /html/)
-            .expect(403)
-            .then(response => {
-                expect(response.text).toBe("The service you want to remove does not exist");
-            })
-    })
-    test("Removing from a non existent counter", async() => {
-        await request(app)
-            .delete('/counters/8/services/1')
-            .expect('Content-Type', /html/)
-            .expect(403)
-            .then(response => {
-                expect(response.text).toBe("The counter you want to modify does not exist");
-            })
-    })
-    test("Trying to remove a service that exists, but is not into the counter", async () => {
-        // add service 1 into counter 2
-        await request(app)
-            .put('/counters/2')
-            .send({ service: 1 });
-        // remove service 1 from counter 2
-        await request(app)
-            .delete('/counters/2/services/1');
-        // remove again service 1 from counter 2
-        await request(app)
-            .delete('/counters/2/services/1')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body).toStrictEqual({});
-            })
-    })
-})
+  test("Removing an existing service from an existing counter", async () => {
+    // add service 1 into counter 2
+    await request(app).put("/counters/2").send({ service: 1 });
+    // remove service 1 from counter 2
+    await request(app)
+      .delete("/counters/2/services/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toStrictEqual({});
+      });
+  });
+  test("Removing a non existent service", async () => {
+    await request(app)
+      .delete("/counters/2/services/7")
+      .expect("Content-Type", /html/)
+      .expect(403)
+      .then((response) => {
+        expect(response.text).toBe(
+          "The service you want to remove does not exist",
+        );
+      });
+  });
+  test("Removing from a non existent counter", async () => {
+    await request(app)
+      .delete("/counters/8/services/1")
+      .expect("Content-Type", /html/)
+      .expect(403)
+      .then((response) => {
+        expect(response.text).toBe(
+          "The counter you want to modify does not exist",
+        );
+      });
+  });
+  test("Trying to remove a service that exists, but is not into the counter", async () => {
+    // add service 1 into counter 2
+    await request(app).put("/counters/2").send({ service: 1 });
+    // remove service 1 from counter 2
+    await request(app).delete("/counters/2/services/1");
+    // remove again service 1 from counter 2
+    await request(app)
+      .delete("/counters/2/services/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toStrictEqual({});
+      });
+  });
+});
