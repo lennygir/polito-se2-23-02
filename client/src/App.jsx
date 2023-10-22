3464a43b5dda18530e8eb21365ab258c2cb056a3
carlosvaleriano <carlosv.2064@gmail.com>
2023-10-22 23:24:51 +0200

    feat: landing page and routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminPage from "./routes/AdminPage";
+import ClientPage from "./routes/ClientPage";
+import ErrorPage from "./routes/ErrorPage";
+import LandingPage from "./routes/LandingPage";
+import OfficerPage from "./routes/OfficerPage";
+import RootPage from "./routes/RootPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <CssBaseline />
+      <Main />
+    </BrowserRouter>
+  );
+}
+
+function Main() {
+  return (
+    <Routes>
+      <Route path="/" element={<RootPage />}>
+        <Route index element={<LandingPage />} />
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="officer/:officerId" element={<OfficerPage />} />
+        <Route path="client/:clientId" element={<ClientPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/routes/AdminPage.jsx
@@ -0,0 +1,3 @@
+export default function AdminPage() {
+  return <div id="admin-page"></div>;
+}
