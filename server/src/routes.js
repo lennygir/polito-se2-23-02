c6ea10fe4bce77f186aff1832d288adb105a0fa6
Luca Tortore <43878433+Luca0049@users.noreply.github.com>
2023-10-24 19:06:32 +0200

    complete getCounters endpoint

--- server/src/routes.js
+++ server/src/routes.js
@@ -38,6 +38,16 @@ router.get("/service/:service/getTicket", (req, res) => {
   res.status(200).json({ data: dataService.data.currentTicketNumber });
 });
 
+//endpoint for returning all the counters
+router.get("/counter/retCounters", (req, res) => {
+  const ret = dataService.data.counters;
+  if(ret.length === 0) {
+      return res.status(404).json({message: `No counters is found`});
+  }
+  
+  res.status(200).json({ data: ret });
+});
+
 // ==================================================
 // Handle 404 not found - DO NOT ADD ENDPOINTS AFTER THIS
 // ==================================================
